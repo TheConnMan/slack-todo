4ce3fdc9e02dc940fd303d5ccf8a69de7592773b
TheConnMan <bcconn2112@gmail.com>
2016-05-07 18:59:11 -0400

    Add team and task models

---
+++ api/models/Team.js
@@ -0,0 +1,26 @@
+/**
+ * Team.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+
+    teamId: {
+      type: 'string',
+      required: true
+    },
+
+    tasks: {
+      collection: 'task',
+      via: 'team'
+    }
+  }
+};
